1ed0c0dfd9329b4a9fe4ca5143626fe858b7855c
morgan greenwalt <you@example.com>
2017-09-10 14:40:22 -0500

    saving shell code

---
+++ app/components/Search.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Search extends React.Component {
+    render() {
+        return(
+        <div className="row">
+            
+        </div>
+        );
+    }
+}
